15cf0e80b47c70ec5b1f1ae25867018ddedf3b5c
j <748557+Jarrio@users.noreply.github.com>
2025-03-03 19:42:33 +0000

    remove some nonsense and changed a small api

--- src/discord_js/GuildManager.hx
+++ src/discord_js/GuildManager.hx
@@ -1,11 +1,11 @@
 package discord_js;
-
+import haxe.extern.EitherType;
 import discord_js.ThreadManager.BaseFetchOptions;
 
 @:jsRequire("discord.js", "GuildManager") extern class GuildManager extends BaseManager<String, Guild, GuildResolvable> {
 	function new(client:Client, ?iterable:Iterable<Dynamic>);
 	public function create(name:String, ?options:GuildCreateOptions):js.lib.Promise<Guild>;
-	public function fetch(?id:FetchGuildOptions):js.lib.Promise<Guild>;
+	public function fetch(?id:EitherType<String, FetchGuildOptions>):js.lib.Promise<Guild>;
 	static var prototype : GuildManager;
 }
 
