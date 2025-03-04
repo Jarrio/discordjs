190b7cdc97f145d1504a127bd1a49823c79bfdee
j <748557+Jarrio@users.noreply.github.com>
2025-03-04 11:14:49 +0000

    update api a bit

--- src/discord_js/ChannelManager.hx
+++ src/discord_js/ChannelManager.hx
@@ -1,5 +1,6 @@
 package discord_js;
 
+import ts.AnyOf3;
 import haxe.extern.EitherType;
 import js.lib.Promise;
 import discord_js.ThreadManager.BaseFetchOptions;
@@ -7,6 +8,6 @@ import discord_js.ThreadManager.BaseFetchOptions;
 @:jsRequire("discord.js", "ChannelManager") extern class ChannelManager extends BaseManager<String, Channel, ChannelResolvable> {
 	function new(client:Client, iterable:Iterable<Dynamic>);
 	@:overload(function(id:String, ?options:BaseFetchOptions):Promise<VoiceChannel>{})
-	public function fetch(id:String, ?options:BaseFetchOptions):Promise<EitherType<TextChannel, VoiceChannel>>;
+	public function fetch(id:String, ?options:BaseFetchOptions):Promise<AnyOf3<TextChannel, ThreadChannel, VoiceChannel>>;
 	static var prototype : ChannelManager;
 }
